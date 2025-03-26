f0df274f0e5b8fc02089413b05011e094898a68e
sweating-bullets <hussainkhanadil459@gmail.com>
2025-03-26 11:49:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ from os import getenv
 
 # VPS --- FILL COOKIES 🍪 in """ ... """ 
 
-INST_COOKIES = """
-# wtite up here insta cookies
-"""
+#INST_COOKIES = """
+# wtite up here insta cookies """
 
-YTUB_COOKIES = """
-# write here yt cookies
-"""
+
+#YTUB_COOKIES = """
+# write here yt cookies"""
+
 
 API_ID = int(getenv("API_ID", "20870930"))
 API_HASH = getenv("API_HASH", "d8339c188abe7b852e52ef2d0d48c770")
@@ -22,9 +22,9 @@ LOG_GROUP = getenv("LOG_GROUP", "-1002308118349")
 CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002217595765"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
-WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
-AD_API = getenv("AD_API", "52b4a2cf4687d81e7d3f8f2b7bc2943f618e78cb")
-STRING = getenv("STRING", None)
-YT_COOKIES = getenv("YT_COOKIES", YTUB_COOKIES)
-DEFAULT_SESSION = getenv("DEFAUL_SESSION", None)  # added old method of invite link joining
-INSTA_COOKIES = getenv("INSTA_COOKIES", INST_COOKIES)
+#WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
+#AD_API = getenv("AD_API", "52b4a2cf4687d81e7d3f8f2b7bc2943f618e78cb")
+#STRING = getenv("STRING", None)
+#YT_COOKIES = getenv("YT_COOKIES", YTUB_COOKIES)
+#DEFAULT_SESSION = getenv("DEFAUL_SESSION", None)  # added old method of invite link joining
+#INSTA_COOKIES = getenv("INSTA_COOKIES", INST_COOKIES)
